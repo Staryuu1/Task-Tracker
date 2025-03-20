b248fb4e02204ac319bc8d0351cac5695304e7de
Mohammad Dwi Becket <becket@Macbook-Pro-Becket.local>
2025-03-20 08:56:02 +0700

    remove folder/file yang tidak di perlukan

--- models/Profile.js
+++ models/Profile.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const ProfileSchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // Pastikan setiap task terkait dengan pengguna
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, 
     name: { type: String, required: true },
     phoneNumber: { type: Number, required: true, default: 0 }, 
 });
